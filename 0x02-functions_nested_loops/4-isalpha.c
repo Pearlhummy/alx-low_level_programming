8a17dfa445e2a99bcbaacd8e2ac9c3ae2e5ce11d
Pearlhummy <igbinladepelumi@gmail.com>
2022-07-05 14:07:24 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - function to know if the character is a letter or not
+ *
+ * @c: variable to evaluate
+ *
+ * Return: Get 1 if it's a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
