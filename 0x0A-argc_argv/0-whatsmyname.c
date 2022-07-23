b407dadf775b0502a92840d67d4497e3bcc87edc
Pearlhummy <igbinladepelumi@gmail.com>
2022-07-22 21:23:20 -0700

    task done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - The main function
+ * @argc: First argument
+ * @argv: Second number
+ * Return: 0;
+ */
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", *argv);
+	return (0);
+}
